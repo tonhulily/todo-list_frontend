2a9f62a771426daeaeaf3471809a1ac47f5a67f7
tonhulily <tonhu170605@gmail.com>
2025-08-11 10:09:56 +0700

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { FaEdit, FaTrash } from 'react-icons/fa';
 
 import './App.css';
 
-const API_URL = 'https://todo-api-go-4ki3.onrender.com';
+const API_URL = 'https://todo-api-go-4ki3.onrender.com/tasks';
 
 function App() {
   const [tasks, setTasks] = useState([]);
